7abc176a66fd191e18f154e5a4114cce67257979
Tyler Bantley <tyler.bantley@icloud.com>
2022-03-16 23:55:26 -0500

    nav routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Bookings from './components/pages/Bookings';
+import Contact from './components/pages/Contact';
+import Team from './components/pages/Team';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' exact component={Services} />
+        <Route path='/bookings' exact component={Bookings} />
+        <Route path='/contact' exact component={Contact} />
+        <Route path='/team' exact component={Team} />
+      </Switch>
     </Router>
      
     
---
+++ src/components/pages/Bookings.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Bookings() {
+    return ( 
+        <>
+        <h1 className="bookings">BOOKINGS</h1>
+        </>
+    )
+}
